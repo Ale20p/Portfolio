d046716f61111f26ded70bdc18ea564a863a5ade
Alessandro Pomponi <alescode1@gmail.com>
2024-12-29 23:00:20 -0500

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="default-layout">
+    <NuxtPage /> 
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+.default-layout {
+  background-color: var(--background-color);
+  color: var(--text-color);
+  height: 100vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main-page">
+    <div class="middle-of-page">
+      <h1>Alessandro Pomponi</h1>
+
+      <p>Computer Science Student</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+.main-page {
+  display: flex;
+  flex-direction: column;
+  /* Fill the entire viewport: */
+  height: 100vh;
+  /* Center child elements both horizontally and vertically: */
+  justify-content: center;
+  align-items: center;
+}
+
+.middle-of-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 40%;
+  width: 100%;
+  background-color: var(--secondary-color);
+}
+</style>
