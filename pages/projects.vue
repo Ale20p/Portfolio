96d2a05b40a84a25d359e4697426b50f228901f2
Alessandro Pomponi <alescode1@gmail.com>
2025-01-02 04:07:27 -0500

    initial setup of projects page

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    
+  <div class="entire-body">
+    <div class="projects-section">
+
+    </div>
   </div>
 </template>
 
@@ -9,5 +11,23 @@
 </script>
 
 <style>
+.entire-body {
+  display: flex; /* Without this, align-items and justify-content do not take affect. */
+  background: url(~/assets/images/projects-top-gold.jpg) top no-repeat,
+              url(~/assets/images/projects-dark-part.jpg) center repeat-y,
+              url(~/assets/images/projects-bottom-gold.jpg) bottom no-repeat;
+  background-size: 100% auto;
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+  align-items: center;
+  justify-content: center;
+}
 
+.projects-section {
+  position: relative;
+  min-height: 100vh;
+  width: 60%;
+  background-color: var(--projects-color);
+}
 </style>
