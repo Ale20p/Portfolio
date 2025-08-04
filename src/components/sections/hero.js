ebcb931337b70712e19b71f408b110c5f6695428
Alessandro Pomponi <alescode1@gmail.com>
2025-08-04 18:01:40 -0400

    small changes to footer, about, and hero sections

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,16 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Alessandro Pomponi.</h2>;
+  const three = <h3 className="big-heading">I build software applications and tools.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m an up and coming software engineer who's passionate about building efficient,
+        user-focused applications that bridge creativity and technology. Currently, I’m working
+        towards obtaining a Bachelor's degree in Software Engineering at{' '}
+        <a href="https://www.concordia.ca" target="_blank" rel="noreferrer">
+          Concordia University
         </a>
         .
       </p>
